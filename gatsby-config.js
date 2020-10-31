882ac45e41d1b5f2f74e7ecd12954b7df7954276
sato-hirokazu <sato-hirokazu@dmm.com>
2020-10-31 18:48:48 +0900

    修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
   },
-  pathPrefix: "/my-blog",
+  //pathPrefix: "/my-blog",
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
